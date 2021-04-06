20150b2b81f9d301b8f0be1d7ce62d9d14e3d268
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-06 12:25:14 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -62,6 +62,11 @@ launchButton.addEventListener("click", function (event) {
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
 
+confirmButton.addEventListener("click", function (event) {
+  modal.classList.add("off");
+  successMessage.classList.remove("off");
+});
+
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
